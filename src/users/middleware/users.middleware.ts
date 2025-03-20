33e1aeedf0e518331ab229af6ec366faf6a67945
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2025-03-20 13:35:21 +0100

    feat: add userSchema

--- src/users/middleware/users.middleware.ts
+++ src/users/middleware/users.middleware.ts
@@ -4,18 +4,21 @@ import {
   BadRequestException,
 } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
+import Joi from 'joi';
 import { validate } from 'uuid';
+
+const userSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  age: Joi.number().required(),
+});
 
 @Injectable()
 export class ValidateUser implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    const { name, email, age } = req.body as {
-      name: string;
-      email: string;
-      age: number;
-    };
+    const { error } = userSchema.validate(req.body, { abortEarly: false });
 
-    if (!name || !email || typeof age !== 'number') {
+    if (error) {
       throw new BadRequestException('Missing required fields');
     }
 
